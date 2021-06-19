7f69c08e58fdfa44790a142f5a6d2034b5be1740
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2021-06-19 15:40:00 +0100

    first

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import './styles/App.css';
 import Header from './pages/Header/Header';
 import Button from "./ui/Button/Button"
 function App() {
+  const name = {
+    "age": 1
+  }
   return (
+
     <div className="App"> 
-    <Header />
-    <h1>Welcome to music box</h1>
+                            <Header />
+    
+    
+          <h1>Welcome to music box, {name.age}
+          
+          </h1>
     <Button />
     </div>
   );
