1a2f5f6f5c4f200d16b2826e56288515c0fd891c
Rotimi <70343818+Lazy-poet@users.noreply.github.com>
2021-06-19 17:48:05 +0100

    Starter template

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,20 @@
 import './styles/App.css';
-import Header from './pages/Header/Header';
-import Button from "./ui/Button/Button"
+import Header from './components/Header/Header';
+import Button from './ui/Button/Button';
+import AuthProvider from './context/AuthContext';
 function App() {
-  
   const name = {
-    "age":      
-    1
-  }
+    age: 1,
+  };
   return (
+    <AuthProvider>
+      <div className='App'>
+        <Header />
 
-    <div className="App"> 
-                            <Header />
-    
-    
-          <h1>Welcome to music box, {name.age}
-          
-          </h1>
-    <Button />
-    </div>
+        <h1>Welcome to music box, {name.age}</h1>
+        <Button />
+      </div>
+    </AuthProvider>
   );
 }
 
---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,22 @@
+import { createContext, useState, ReactNode, Dispatch, SetStateAction } from 'react';
+
+// This is just a dummy context file which exports loggedIn state and function to set LoggedIn status
+
+interface Props {
+  children: ReactNode;
+}
+
+interface AuthStatus {
+  isLoggedIn: boolean;
+  setIsLoggedIn: Dispatch<SetStateAction<boolean>>;
+}
+
+export const AuthContext = createContext({} as AuthStatus);
+
+const AuthProvider = (props: Props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
+
+  return <AuthContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>{props.children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
