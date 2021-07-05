208090c3a80361e9c8a7b06cf593084b8f671e50
Benedict <jokanolaadedipupo@gmail.com>
2021-07-05 10:42:26 +0100

    changes

--- src/components/NoResult/NoResult.tsx
+++ src/components/NoResult/NoResult.tsx
@@ -1,24 +1,23 @@
 import React, { Dispatch, SetStateAction } from 'react';
-import { Modal} from 'react-bootstrap';
+import { Modal } from 'react-bootstrap';
+import styles from './NoResult.module.css';
 interface Props {
   //declare props here
   show: boolean;
   setShow: Dispatch<SetStateAction<boolean>>;
 }
 
-
 const NoResult = ({ show, setShow }: Props) => {
-
   const handleClose = () => setShow(false);
-  // const handleShow = () => setShow(true);
   return (
     <>
-
       <Modal show={show} onHide={handleClose}>
-        <Modal.Header closeButton>
-          
-        </Modal.Header>
-        <Modal.Body>No Result!, try again</Modal.Body>
+        <Modal.Header closeButton style={{ border: 'none' }}></Modal.Header>
+        <Modal.Body className={styles.modalContent}>
+          <h2 className={styles.noResult}>No Result</h2>
+          <h4 className={styles.text}>MusicFInder didn't quite catch that</h4>
+          <h5 className={styles.text2}>Try Again</h5>
+        </Modal.Body>
       </Modal>
     </>
   );
