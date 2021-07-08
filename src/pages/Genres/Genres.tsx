69e085b96637204df874ca998c92fa60fee85c00
dimola7 <didiogidan@gmail.com>
2021-07-09 00:48:46 +0100

    added loaders

--- src/pages/Genres/Genres.tsx
+++ src/pages/Genres/Genres.tsx
@@ -1,18 +1,14 @@
 import React from 'react';
 import GenreCard from '../../components/GenreCard/GenreCard';
 import genreStyles from './Genres.module.css';
-import { motion } from 'framer-motion';
-import { pageTransition, transit } from '../../utils/animate';
 
 const Genres = () => {
   return (
-    <motion.div initial='out' animate='in' variants={pageTransition} transition={transit}>
       <div className={genreStyles.genreBody}>
         <div>
           <GenreCard />
         </div>
       </div>
-    </motion.div>
   );
 };
 
