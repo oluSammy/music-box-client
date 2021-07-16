e73e71929a853fe8e8ab9f7588849ee4e8f4fdf4
chubbystrings <48825855+chubbystrings@users.noreply.github.com>
2021-07-16 21:09:04 +0100

    added loader to artistPage

--- src/pages/SIngleArtist/SIngleArtist.tsx
+++ src/pages/SIngleArtist/SIngleArtist.tsx
@@ -15,6 +15,7 @@ import MoreVertIcon from '@material-ui/icons/MoreVert';
 import useMusicPlayer from '../../hooks/useMusicPlayer';
 import { motion } from 'framer-motion';
 import { pageTransition, transit } from '../../utils/animate';
+import Loader from '../../ui/Loader/Loader';
 
 const SIngleArtist = () => {
   const ctx = useContext(AuthContext);
@@ -32,7 +33,7 @@ const SIngleArtist = () => {
   useEffect(() => {
     try {
       const fetchArtist = async () => {
-        setIsLoading(true);
+        // setIsLoading(true);
         try {
           const {
             data: { data },
@@ -50,8 +51,10 @@ const SIngleArtist = () => {
           setArtistName(`${data.artist.name}-${data.artist.id}`);
           setArtistMongoId(data.artist._id);
           setArtist(data);
+          setIsLoading(false);
         } catch (e) {
           console.log(e, 'ERROR');
+          setIsLoading(false);
         }
       };
       try {
@@ -89,81 +92,82 @@ const SIngleArtist = () => {
 
   return (
     <>
-    {artist &&
-      <motion.div initial='out' animate='in' exit='out' variants={pageTransition} transition={transit}>
-        <div className={artistStyles.artistBody}>
-          <div className={artistStyles.mobileIcons}>
-            <div>
-              <ArrowBackIcon />
-            </div>
-            <div className={artistStyles.right}>
-              <ShareIcon />
-              <MoreVertIcon />
-            </div>
-          </div>
-          <div className={artistStyles.artistGrid}>
-            <div className={artistStyles.artistFlex}>
+      {isLoading && <Loader />}
+      {!isLoading && artist && (
+        <motion.div initial='out' animate='in' exit='out' variants={pageTransition} transition={transit}>
+          <div className={artistStyles.artistBody}>
+            <div className={artistStyles.mobileIcons}>
               <div>
-                <img src={artist?.artist?.picture} className={artistStyles.artistImage} alt='' />
+                <ArrowBackIcon />
               </div>
-              <div className={artistStyles.artistDets}>
-                <p className={artistStyles.artistTitle}>Artist</p>
-                <div className={artistStyles.artistName}>{artist?.artist?.name}</div>
-                <div className={artistStyles.buttons}>
-                  <button
-                    className={artistStyles.followButton}
-                    onClick={() => {
-                      likeArtist();
-                    }}
-                    style={{ color: like ? 'red' : 'white', borderColor: like ? 'red' : 'white' }}
-                  >
-                    <span>
-                      <FavoriteBorderIcon style={{ fontSize: 'medium', fill: like ? 'red' : 'white' }} />
-                    </span>{' '}
-                    <span>Like</span>
-                  </button>
-                  <button
-                    className={artistStyles.shuffleButton2}
-                    onClick={() => {
-                      handleSongClick(artist.songs[0].id, artist.songs);
-                      handleShuffle();
-                      console.log('clicked');
-                    }}
-                  >
-                    <ShuffleIcon style={{ fontSize: 'medium' }} /> shuffle play
-                  </button>
+              <div className={artistStyles.right}>
+                <ShareIcon />
+                <MoreVertIcon />
+              </div>
+            </div>
+            <div className={artistStyles.artistGrid}>
+              <div className={artistStyles.artistFlex}>
+                <div>
+                  <img src={artist?.artist?.picture} className={artistStyles.artistImage} alt='' />
+                </div>
+                <div className={artistStyles.artistDets}>
+                  <p className={artistStyles.artistTitle}>Artist</p>
+                  <div className={artistStyles.artistName}>{artist?.artist?.name}</div>
+                  <div className={artistStyles.buttons}>
+                    <button
+                      className={artistStyles.followButton}
+                      onClick={() => {
+                        likeArtist();
+                      }}
+                      style={{ color: like ? 'red' : 'white', borderColor: like ? 'red' : 'white' }}
+                    >
+                      <span>
+                        <FavoriteBorderIcon style={{ fontSize: 'medium', fill: like ? 'red' : 'white' }} />
+                      </span>{' '}
+                      <span>Like</span>
+                    </button>
+                    <button
+                      className={artistStyles.shuffleButton2}
+                      onClick={() => {
+                        handleSongClick(artist.songs[0].id, artist.songs);
+                        handleShuffle();
+                        console.log('clicked');
+                      }}
+                    >
+                      <ShuffleIcon style={{ fontSize: 'medium' }} /> shuffle play
+                    </button>
+                  </div>
                 </div>
               </div>
+              <div className={artistStyles.right}>
+                <button
+                  className={artistStyles.shuffleButton}
+                  onClick={() => {
+                    handleSongClick(artist.songs[0].id, artist.songs);
+                    handleShuffle();
+                    console.log('clicked');
+                  }}
+                >
+                  shuffle play
+                </button>
+                <span
+                  className={artistStyles.icons}
+                  onClick={() => {
+                    likeArtist();
+                  }}
+                >
+                  <MdFavoriteBorder style={{ fill: like ? 'red' : 'white', borderColor: like ? 'red' : 'white' }} />
+                </span>
+                <span className={artistStyles.icons}>
+                  <RiMoreLine />
+                </span>
+              </div>
             </div>
-            <div className={artistStyles.right}>
-              <button
-                className={artistStyles.shuffleButton}
-                onClick={() => {
-                  handleSongClick(artist.songs[0].id, artist.songs);
-                  handleShuffle();
-                  console.log('clicked');
-                }}
-              >
-                shuffle play
-              </button>
-              <span
-                className={artistStyles.icons}
-                onClick={() => {
-                  likeArtist();
-                }}
-              >
-                <MdFavoriteBorder style={{ fill: like ? 'red' : 'white', borderColor: like ? 'red' : 'white' }} />
-              </span>
-              <span className={artistStyles.icons}>
-                <RiMoreLine />
-              </span>
-            </div>
+            <ArtistPopularSongs artist={artist} isLoading={isLoading} error={error} artistId={artistMongoId} />
+            <ArtistAlbums artist={artist} />
           </div>
-          <ArtistPopularSongs artist={artist} isLoading={isLoading} error={error} artistId={artistMongoId} />
-          <ArtistAlbums artist={artist}/>
-        </div>
-      </motion.div>
-    }
+        </motion.div>
+      )}
     </>
   );
 };
